b1586229a1a81c90a02b45076b09f2507eb743aa
YSpoof <32226366+YSpoof@users.noreply.github.com>
2026-09-17 14:40:34 -0300

    Fix: Adjust tsdown exports

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -6,4 +6,16 @@ export default defineConfig({
   dts: true,
   clean: true,
   minify: true,
+  fixedExtension: true,
+  exports: {
+    customExports(exports) {
+      exports["./client"] = { types: "./client.d.ts" };
+      exports["./tsconfig/service-worker"] = "./tsconfig.sw.json";
+      exports["./sw"] = "./tsconfig.sw.json";
+      return exports;
+    },
+  },
+  deps: {
+    neverBundle: ["vite", "esbuild"],
+  },
 });
